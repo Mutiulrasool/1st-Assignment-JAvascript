a3914d45841a0bfac586a74ce0804770390ad3aa
Mutiulrasool <138636782+Mutiulrasool@users.noreply.github.com>
2023-10-10 03:27:03 -0700

    Add files via upload
    This is First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Error! Please Enter a valid password"
+    )
+alert("Welcome to JS land\nHappy Code "
+    )
+alert("Welcome to JS land...."
+    )
+alert("Happy coding\nprevent this page from creating additional diolouge"
+ )
+alert(" I can run javascript Through my web browser")
